ff6e4c92bbf15798c75e87c901098c2121bff814
David Roqueni <defconm15@gmail.com>
2018-08-05 21:43:58 -0700

    Initial creation of app

---
+++ AZEM/AZEM/AZEM/AppStyles/AppStyle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AZEM.AppStyles
+{
+    class AppStyle : Application
+    {
+        #region Colors
+        private static readonly Color lightBlue = Color.FromHex("#2EB2FF");
+        private static readonly Color darkBlue = Color.FromHex("#4259CB");
+        #endregion
+        private static readonly String font = "Roboto.ttf";
+        #region styles
+        #endregion
+    }
+}
